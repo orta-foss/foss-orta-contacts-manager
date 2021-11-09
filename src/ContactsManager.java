54637cf21ecdd12353c8f6ac629d0aa456c96ba0
Kevin Orta <orta.kevin@gmail.com>
2021-11-09 12:33:45 -0600

    refactoring try/catch

--- src/ContactsManager.java
+++ src/ContactsManager.java
@@ -3,37 +3,33 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class ContactsManager {
 
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String directory = "./src/text";
         String fileName = "contactsList.txt";
         Path textDirectory = Paths.get(directory);
-        Path textFile = Paths.get(directory, fileName);
+        Path textPath = Paths.get(directory, fileName);
 
-        try {
             if (Files.notExists(textDirectory)) {
                 Files.createDirectory(textDirectory);
             }
-        } catch (IOException e) {
-            System.out.println(("Oops, we didn't find the file correctly..."));
-            e.printStackTrace();
-        }
 
-        try {
-            if (Files.notExists(textFile)) {
-                Files.createFile(textFile);
+            if (Files.notExists(textPath)) {
+                Files.createFile(textPath);
             }
-        } catch (IOException e) {
-            System.out.println(("Oops, we didn't find the file correctly..."));
-            e.printStackTrace();
-        }
 
+        Scanner scanner = new Scanner(System.in);
         //Checks to see if we have the text file in our system
-        System.out.println(Files.exists(textFile));
+        System.out.println(Files.exists(textPath));
+        List<String> contacts = Arrays.asList("Joanna");
+        Files.write(textPath, contacts);
 
 
         System.out.println("Hello, welcome to your Contacts Manager, choose from the follow options: ");
@@ -43,6 +39,19 @@ public class ContactsManager {
         System.out.println("4. Delete an existing contact");
         System.out.println("5. Exit");
         System.out.println("Enter an option (1, 2, 3, 4 or 5):");
+        int userChoice = scanner.nextInt();
+
+        switch (userChoice){
+            case 1:
+                try {
+                List<String> printList = Files.readAllLines(textPath);
+                    System.out.println(printList);
+                } catch (IOException e){
+                System.out.println("Could not find your file..");
+                e.printStackTrace();
+            }
+
+        }
 
 
     }
